59566d40423d75c3469a56f6cc48adc5fa2e4200
eweiberl <eweiberl@student.42heilbronn.de>
2024-02-26 15:59:30 +0100

    added ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+	int			str_num = 1;
+	std::string	str;
+
+	if (argc <= 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	while (str_num < argc)
+	{
+		int	i = 0;
+		str = argv[str_num];
+		while (str[i] != '\0')
+		{
+			str[i] = std::toupper(str[i]);
+			i++;
+		}
+		std::cout << str;
+		str_num++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
+//std::cout <<
