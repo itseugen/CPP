56f4c3f4e9b7b0ba1d183e6eb7c2096cf7202100
eweiberl <eweiberl@student.42heilbronn.de>
2024-02-27 18:01:54 +0100

    Lowercase input works too

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -9,6 +9,8 @@ int	main(void)
 	{
 		std::cout << "ADD to add a contact | SEARCH to look up a contact | EXIT to stop the program: ";
 		std::getline(std::cin, input);
+		for (size_t i = 0; i < input.size(); i++)
+			input[i] = std::toupper(input[i]);
 		if (input == "ADD")
 		{
 			pbook.newContact();
