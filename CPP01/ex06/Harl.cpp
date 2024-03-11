149ac0b2f1e53607d1d20c7c3e006483b3c9bc1d
eweiberl <eweiberl@student.42heilbronn.de>
2024-03-11 17:36:21 +0100

    added files

---
+++ CPP01/ex06/Harl.cpp
@@ -0,0 +1,48 @@
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+	std::cout << "Hello World!" << std::endl;
+}
+
+Harl::~Harl()
+{
+	std::cout << "NOOO WHY DON'T DESTROY ME!! AHHHH..." << std::endl;
+}
+
+void	Harl::complain(std::string level)
+{
+	const char *names[] = {"DEBUG", "INFO", "WARNING", "ERROR", NULL};
+	void (Harl::*msg[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	for (int i = 0; level[i]; i++)
+		level[i] = std::toupper(level[i]);
+	for (int i = 0; i < 4; i++)
+	{
+		if (names[i] == level)
+		{
+			(this->*msg[i])();
+			return ;
+		}
+	}
+	std::cout << "I ONLY ACCEPT DEBUG, INFO, WARNING, ERROR!!!!!" << std::endl;
+}
+
+void	Harl::debug(void)
+{
+	std::cout << "This is a debug message, why do I have to debug myself again?" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "This is an info message. Why do I even keep you informed?" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "This is a warning. Listen or your Computer has Virus!" << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "ERROR! FATAL! PREPARE FIRE EXTINGUISHER!" << std::endl;
+}
---
+++ CPP01/ex06/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.hpp"
+
+int	main(void)
+{
+	Harl	complainer;
+
+	complainer.complain("TEST");
+	complainer.complain("DEBUG");
+	complainer.complain("Info");
+	complainer.complain("Warning");
+	complainer.complain("ERROR");
+	return (0);
+}
