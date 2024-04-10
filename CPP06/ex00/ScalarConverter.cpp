82f6d9e87919d465e4b51606c74e5f1311d3dd5b
Eugen <118663096+itseugen@users.noreply.github.com>
2024-04-10 13:07:14 +0000

    basic impkementation

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -29,7 +29,38 @@ ScalarConverter&	ScalarConverter::operator=(const ScalarConverter &copy)
 	return (*this);
 }
 
-void	ScalarConverter::convert(std::string& str)
+void	ScalarConverter::convert(const std::string& str)
 {
-	
+	ScalarConverter		converter;
+	long double			nbr = 0;
+	std::istringstream	iss(str);
+
+	iss >> nbr;
+	if (str.length() == 1)
+	{
+		char	a = str[0];
+
+		nbr = static_cast<int>(a);
+	}
+	else if (str.length() > 1 && !std::isdigit(str[0]))
+	{
+		std::cout << "Char: " << "Conversion not possible!\n";
+		std::cout << "Int: " << "Conversion not possible!\n";
+		std::cout << "Float: " << "Conversion not possible!\n";
+		std::cout << "Double: " << "Conversion not possible!\n";
+		return ;
+	}
+	converter.printChar(nbr);
+	std::cout << "Int: " << static_cast<int>(nbr) << std::endl;
+	std::cout << "Float: " << static_cast<float>(nbr) << std::endl;
+	std::cout << "Double: " << static_cast<double>(nbr) << std::endl;
+}
+
+void	ScalarConverter::printChar(long double nbr)
+{
+	if (nbr < 0 || nbr > 256 || !std::isprint(static_cast<int>(nbr)))
+	{
+		std::cout << "Char: " << "Conversion not possible!\n";
+	}
+	std::cout << "Char: " << static_cast<int>(nbr) << std::endl;
 }
--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -3,17 +3,20 @@
 
 # include <iostream>
 # include <string>
+# include <sstream>
 
 class ScalarConverter
 {
 	private:
 		ScalarConverter();
+
+		void	printChar(long double nbr);
 	public:
 		~ScalarConverter();
 		ScalarConverter(const ScalarConverter& copy);
 		ScalarConverter& operator=(const ScalarConverter& copy);
 
-		static void	convert(std::string& str);
+		static void	convert(const std::string& str);
 };
 
 #endif /*SCALARCONVERTER_HPP*/
