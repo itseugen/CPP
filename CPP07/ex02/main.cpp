6ea2239d25c385bc6e28d24fcdccd774578a458a
itseugen <eugen.weiberle@gmail.com>
2024-04-22 17:12:53 +0200

    added test

--- CPP07/ex02/main.cpp
+++ CPP07/ex02/main.cpp
@@ -6,6 +6,7 @@
 static void	test01(void);
 static void	test02(void);
 static void	test03(void);
+static void	test04(void);
 static void	incr(int& i);
 static void	squareRoot(int &i);
 
@@ -29,6 +30,8 @@ int main(void)
 	test02();
 	std::cout << "\n\nTest03:\n";
 	test03();
+	std::cout << "\n\nTest04:\n";
+	test04();
 	return 0;
 }
 
@@ -71,6 +74,29 @@ static void	test03(void)
 		Array<int>	arr(-10);
 
 		arr[2] = -2;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Exception: " << e.what() << '\n';
+	}
+}
+
+static void	test04(void)
+{
+	try
+	{
+		Array<int>	arr(20);
+		Array<int>	arr2(40);
+
+		arr2 = arr;
+		for (size_t i = 0; i < arr.size(); i++)
+			arr[i] = i + 10;
+		for (size_t i = 0; i < arr.size(); i++)
+			arr2[i] = i - 10;
+		std::cout << "Printing arr: ";
+		printArr(arr);
+		std::cout << "Printing arr2: ";
+		printArr(arr2);
 	}
 	catch(const std::exception& e)
 	{
