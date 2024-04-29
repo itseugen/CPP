a3a41107e2a28ac427a1b31fcd7991ed5455784e
Eugen <118663096+itseugen@users.noreply.github.com>
2024-04-29 14:03:05 +0000

    program takes input file now!

--- CPP09/ex00/main.cpp
+++ CPP09/ex00/main.cpp
@@ -1,13 +1,18 @@
 #include  "BitcoinExchange.hpp"
 
-int	main(void)
+int	main(int argc, char* argv[])
 {
-	BitcoinExchange	btc;
-
+	if (argc != 2)
+	{
+		std::cerr << "Wrong input! Program takes ONE file as input!\n";
+		return (1);
+	}
 	try
 	{
+		BitcoinExchange	btc;
+
 		btc.input_database("data.csv");
-		btc.input_file("input");
+		btc.input_file(argv[1]);
 	}
 	catch(const std::exception& e)
 	{
