a7d63183e2ad966f8fca535e7260e40910f1be55
itseugen <eugen.weiberle@gmail.com>
2024-05-15 14:54:53 +0200

    Changed findClosestDate to only take the lower date

--- CPP09/ex00/BitcoinExchange.cpp
+++ CPP09/ex00/BitcoinExchange.cpp
@@ -99,7 +99,7 @@ void	BitcoinExchange::input_file(std::string filename)
 		{
 			if (legal_date(date) == false)
 				std::cout << "Error: bad input => " << line << std::endl;
-			else if (std::fabs(bitcoin_value) > 1.0e15)
+			else if (std::fabs(bitcoin_value) > INT_MAX)
 				std::cout << "Error: too large a number." << std::endl;
 			else if (bitcoin_value < 0)
 				std::cout << "Error: not a positive number!" << std::endl;
@@ -142,7 +142,6 @@ void	BitcoinExchange::calc_value(std::string date, double bitcoin_value)
 	std::cout << date << " => " << bitcoin_value << " = " << res << std::endl;
 }
 
-//! Using the closest date (makes sense because the graph might be more correct, maybe change)
 std::string	BitcoinExchange::findClosestDate(const std::string& inputDate)
 {
 	double		minDifference = std::numeric_limits<double>::max();
@@ -151,14 +150,15 @@ std::string	BitcoinExchange::findClosestDate(const std::string& inputDate)
 	for (std::map<std::string, double>::const_iterator iter = database.begin(); iter != database.end(); ++iter)
 	{
 		const std::string&	date = iter->first;
-		double				difference = fabs(compareDates(date, inputDate));
+		double				difference = compareDates(date, inputDate);
 
-		if (difference < minDifference)
+		if (difference <= 0 && std::fabs(difference) < minDifference)
 		{
-			minDifference = difference;
+			minDifference = std::fabs(difference);
 			closestDate = date;
 		}
 	}
+	// std::cout << "closest date to: " << inputDate << " is: " << closestDate << std::endl;
 	return (closestDate);
 }
 
@@ -174,3 +174,36 @@ double	BitcoinExchange::compareDates(const std::string& date1, const std::string
 
 	return (difference);
 }
+
+// double BitcoinExchange::compareDates(const std::string& date1, const std::string& date2)
+// {
+//     // Parse dates
+//     int y1 = 0, m1 = 0, d1 = 0;
+//     int y2 = 0, m2 = 0, d2 = 0;
+
+//     sscanf(date1.c_str(), "%d-%d-%d", &y1, &m1, &d1);
+//     sscanf(date2.c_str(), "%d-%d-%d", &y2, &m2, &d2);
+
+//     // Convert dates to days since epoch
+//     time_t time1, time2;
+//     struct tm tm1 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // Initialize all fields to 0
+//     struct tm tm2 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // Initialize all fields to 0
+
+//     tm1.tm_year = y1 - 1900; // years since 1900
+//     tm1.tm_mon = m1 - 1;     // months since January
+//     tm1.tm_mday = d1;        // day of the month
+
+//     tm2.tm_year = y2 - 1900; // years since 1900
+//     tm2.tm_mon = m2 - 1;     // months since January
+//     tm2.tm_mday = d2;        // day of the month
+
+//     time1 = mktime(&tm1);
+//     time2 = mktime(&tm2);
+
+//     // Calculate difference in days
+//     double difference = difftime(time1, time2) / (60 * 60 * 24);
+
+//     return difference;
+// }
+
+
